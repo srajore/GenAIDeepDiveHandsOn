4bee69b8ee3bebd9dc9d8e91df90415d4ead6865
Sharad <sharad.rajore@zensar.com>
2025-09-19 21:02:48 +0530

    created env

--- openai_demo.py
+++ openai_demo.py
@@ -2,13 +2,27 @@ from openai import OpenAI
 
 from dotenv import load_dotenv
 
+import os
+
 load_dotenv()
 
-client = OpenAI()
+key = os.getenv("SHARAD_RAJORE_KEY")  
 
-response = client.responses.create(
+client1 = OpenAI(api_key=key)  
+
+client2 = OpenAI()  
+
+          
+
+response1 = client1.responses.create(
     model="gpt-4.1-mini",
     input="What is GenAI in simple terms in 2 lines"
 )
 
-print(response.output_text)
+response2 = client1.responses.create(
+    model="gpt-4.1-mini",
+    input="What is GenAI in simple terms"
+)
+print(response1.output_text)
+
+print(response2.output_text)
